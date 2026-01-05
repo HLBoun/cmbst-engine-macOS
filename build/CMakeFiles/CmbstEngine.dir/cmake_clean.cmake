be9cb01eb17c9778e0dd2eda0ec556bc8acdb073
Legend Boun <legendboun@Legends-MacBook-Air.local>
2026-01-04 23:38:32 -0600

    created MacOS version of Cmbst. I really am the goat

---
+++ build/CMakeFiles/CmbstEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CmbstEngine.dir/src/main.cpp.o"
+  "CMakeFiles/CmbstEngine.dir/src/main.cpp.o.d"
+  "CmbstEngine"
+  "CmbstEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CmbstEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
